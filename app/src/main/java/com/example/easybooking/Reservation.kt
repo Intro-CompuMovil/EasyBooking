ea792000f0a7b1cc7593f47bb9fd57ad87fb03d8
Axell5 <axelcaro034@gmail.com>
2024-03-04 17:04:06 -0500

    Reservas-Uber

---
+++ app/src/main/java/com/example/easybooking/Reservation.kt
@@ -0,0 +1,31 @@
+package com.example.easybooking
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Reservation(val name: String, val description: String) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(description)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Reservation> {
+        override fun createFromParcel(parcel: Parcel): Reservation {
+            return Reservation(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Reservation?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}

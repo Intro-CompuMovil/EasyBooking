ef2c098f8c88881794d0db4cdb510294bb1e7988
Sebootas <Dupersebas@gmail.com>
2024-04-12 21:58:42 -0500

    Map with restaurants and hotels

--- app/src/main/java/com/example/easybooking/MapsFragment.kt
+++ app/src/main/java/com/example/easybooking/MapsFragment.kt
@@ -81,6 +81,74 @@ class MapsFragment : Fragment() {
 
         val BastionBistro = LatLng(10.42837055228262, -75.54584007577178) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
         googleMap.addMarker(MarkerOptions().position(BastionBistro).title("Bastion Bistro"))
+
+        val HotelTermalesdelOtoño = LatLng(5.008669170725944, -75.44018242052084) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(HotelTermalesdelOtoño).title("Hotel Termales del Otoño"))
+
+        val Portón = LatLng(4.664558586137388, -74.04850299026671) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Portón).title("Hotel Portón Bogotá"))
+
+        val CartagenaBeachResort = LatLng(10.371269556935491, -75.56647868833745) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(CartagenaBeachResort).title("Cartagena Beach Resort"))
+
+        val MedellínDowntownSuites = LatLng(6.202540915490962, -75.57142345273303) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(MedellínDowntownSuites).title("Medellín Downtown Suites"))
+
+        val Oceanview = LatLng(11.200704373374228, -74.22722343216455) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Oceanview).title("Santa Marta Ocean view Hotel"))
+
+        val CaliRiverSuites = LatLng(3.4514215654311338, -76.53873460689097) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(CaliRiverSuites).title("Cali River Suites\n"))
+
+        val Portobelo = LatLng(12.585929707204897, -81.69672737560124) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Portobelo).title("Hotel Portobelo Beach"))
+
+        val Pereira = LatLng(4.738571578975511, -75.72585478326043) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Pereira).title("Pereira Mountain Lodge"))
+
+        val Barranquilla = LatLng(10.99862841448146, -74.80026474477769) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Barranquilla).title("Barranquilla Grand Plaza"))
+
+        val Leyva = LatLng(5.63284008104834, -73.52194878145588) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Leyva).title("Villa de Leyva Boutique Hotel"))
+
+        val Colonial = LatLng(6.208759028515478, -75.57211625769888) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Colonial).title("Popayán Colonial Retreat"))
+
+        val QUO  = LatLng(5.053500308274392, -75.48886874757315) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(QUO).title("QUO Quality Hotel"))
+
+        val Pasto = LatLng(1.2244324624198364, -77.28112718027657) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Pasto).title("Lakeside Resort"))
+
+        val Center = LatLng(7.925545828025628, -72.49315036031867) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Center).title("City Center Hotel"))
+
+        val Armenia = LatLng(4.54902424362554, -75.66017965340937) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Armenia).title("Vineyard Retreat"))
+
+        val Neiva = LatLng(2.960069739077076, -75.29747061931306) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Neiva).title("Desert Oasis"))
+
+        val Montería = LatLng(8.758619793206856, -75.88192093133453) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Montería).title("Riverside Hotel,"))
+
+        val Sincelejo = LatLng(9.30145482017214, -75.39531169415343) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Sincelejo).title("Urban Retreat"))
+
+        val Cid = LatLng(5.533733766720032, -73.36169085987493) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Cid).title("Mountain View Lodge"))
+
+        val Riohacha = LatLng(11.521127153261045, -72.96428599655498) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Riohacha).title("Coastal Resort"))
+
+        val Valledupar = LatLng(10.47989859881575, -73.24551989128823) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Valledupar).title("Cultural Hotel"))
+
+        val Jones = LatLng(4.6459297302809865, -74.0602420344055) // Latitude and longitude for "Calle 70 #4-47, Cl. 70 #4 - 47, Bogotá"
+        googleMap.addMarker(MarkerOptions().position(Jones).title("Suite Jones Hotel"))
+
+
     }
 
 
